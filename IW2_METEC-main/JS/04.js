75c0a7904cd1be395f5b38335646b380c3eafb96
luis160209 <luis.miranda21@etec.sp.gov.br>
2025-03-17 10:19:09 -0300

    Add files via upload

---
+++ IW2_METEC-main/JS/04.js
@@ -0,0 +1,15 @@
+// criar função javascript 
+
+// 1º função nomeada
+
+function PegarNumero( ){
+    // DOM (Documento de Objeto Modelo)
+    let numero01 =document.getElementById("n_01");
+     numero01.style.color="red";
+     numero01.style.border =" 1px solid black";
+     numero01.style.backgroundColor ="green";
+     
+     alert(numero01.value);
+     console.log(numero01.value);
+
+}
---
+++ IW2_METEC-main/README.md
@@ -0,0 +1,2 @@
+# IW2_METEC
+Aula de JavaScript pelo curso de técnico de informática para internet 

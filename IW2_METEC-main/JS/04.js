a647c7385fd12ac0e4f65cdfd07346a82684019b
luis160209 <luis.miranda21@etec.sp.gov.br>
2025-04-11 11:52:20 -0300

    Add files via upload

--- IW2_METEC-main/JS/04.js
+++ IW2_METEC-main/JS/04.js
@@ -2,34 +2,14 @@
 
 // 1º função nomeada
 
-let numero01 =document.getElementById("n_01");
-let totalp = document.getElementsByTagName("p");
 function PegarNumero( ){
     // DOM (Documento de Objeto Modelo)
- 
-     numero01.style.color="#ffdd8c";
+    let numero01 =document.getElementById("n_01");
+     numero01.style.color="red";
      numero01.style.border =" 1px solid black";
-     numero01.style.backgroundColor ="green"; 
-     let total = eval(numero01.value);
-     //alert (total);
-     //console.log(numero01.value);
-     totalp[0].innerText = total
+     numero01.style.backgroundColor ="green";
+     
+     alert(numero01.value);
+     console.log(numero01.value);
 
 }
-
-// arrow function
-
- const Adicao=()=>{
-    // numero01.value = numero01.value + "+";
-    numero01.value = numero01.value += "+";
-
-    alert(numero01.value);
-
- }
- const Sub=()=>{
-    // numero01.value = numero01.value + "+";
-    numero01.value = numero01.value += "-";
-
-    alert(numero01.value);
-
- }
